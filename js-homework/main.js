e27357d40f2ed3b86f4262816d479e97b10059fb
DashaBokatova <115945842+DashaBokatova@users.noreply.github.com>
2022-11-01 16:13:17 +0300

    Add files via upload

---
+++ js-homework/main.js
@@ -0,0 +1,52 @@
+let asdqwe = 'hello';
+console.log(asdqwe);
+
+let asdqw = 'owu';
+console.log(asdqw);
+
+let asdq = 'com';
+console.log(asdq);
+
+let asd = 'ua';
+console.log(asd);
+
+let number = 1;
+console.log(number);
+
+let numbe = 10;
+console.log(numbe);
+
+let numb = -999;
+console.log(numb);
+
+let num = 123;
+console.log(num);
+
+const PI = 3.14;
+console.log(PI);
+
+let nu = 2.7;
+console.log(nu);
+
+let n = 16;
+console.log(n);
+
+let f = false;
+console.log(f)
+
+let t = true;
+console.log(t)
+
+let firstName = 'Dasha';
+let middleName = 'Olegovna'
+let lastName = 'Bokatova';
+console.log(`${firstName} ${middleName} ${lastName}`);
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
